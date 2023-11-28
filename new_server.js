60e762c4e21501babf3a3e687c87db847f1281d6
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-11-29 01:41:00 +0530

    authenticate a user from register page

--- new_server.js
+++ new_server.js
@@ -1,9 +1,54 @@
+// if (process.env.NODE_ENV!=="production"){
+//     require("dotenv").config()
+// }
+
+
 const express=require("express");
 const app=express();
+const passport=require("passport");
+const bcrypt=require("bcrypt");
+const localStrategy=require("passport-local").Strategy;
+const session=require("express-session");
+const users=[];
+
+const initializePassport=(passport,getUserbyEmail,getUserbyId)=>{
+    const authenticateUser= async (email,password,done)=>{
+        const user= await getUserbyEmail(email);
+        if (user==null){
+            return done(null,false,{message: "No user with this mail exists"})
+        }
+       
+        try{
+        
+            if ( await bcrypt.compare(password,user.password)){
+            return done(null,user);  
+            }else{
+            return done(null,false,{message: "password incorrect"})
+            }
+         }
+        catch(err){
+           return done(err);
+           }   
+        }
+     passport.use(new localStrategy({usernameField:"email" },authenticateUser))           
+     passport.serializeUser((user,done)=> done(null,user.id));
+     passport.deserializeUser((id,done)=>done(null,getUserbyId));
+    }
+
+initializePassport(passport,email=> users.find(user=>user.email===email),
+    id => users.find(user=>user.id===id))
 
 app.set("view-engine","ejs");
 
 app.use(express.urlencoded({extended:false}));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/",(req,res)=>{
     res.render("index.ejs",{name:"Abhishek"})
@@ -12,5 +57,29 @@ app.get("/register",(req,res)=>{
     res.render("register.ejs")
 })
 
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash:true
+}))
+
+app.post("/register",async(req,res)=>{
+
+    try {
+        const hashedPassword= await bcrypt.hash(req.body.password,10);
+        users.push({
+         id: Date.now().toString(),
+         name: req.body.name,
+         email: req.body.email,
+         password: hashedPassword  
+        })
+        res.redirect("/login");
+    } catch {
+        res.redirect("/register");
+    }
+})
 
 app.listen(3000);
